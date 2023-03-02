0eba15ebd94a38de3dc67745bee1ec808e35a187
bestfriendai <120160378+bestfriendai@users.noreply.github.com>
2023-03-01 19:23:49 -0500

    Update footer.js

--- components/footer.js
+++ components/footer.js
@@ -7,31 +7,10 @@ export default function Footer({ events }) {
 
       <div className="text-center lil-text mt-8">
         Powered by{" "}
-        <Link href="https://github.com/lllyasviel/ControlNet" target="_blank">
-          ControlNet
+        <Link href="https://ShibariumAI.com" target="_blank">
+          ShibariumAI.com
         </Link>{" "}
-        by{" "}
-        <Link
-          href="https://lllyasviel.github.io/Style2PaintsResearch/lvmin"
-          target="_blank"
-        >
-          Lyumin Zhang
-        </Link>
-        ,{" "}
-        <Link
-          href="https://replicate.com/jagilley/controlnet-scribble?utm_source=project&utm_campaign=scribblediffusion"
-          target="_blank"
-        >
-          Replicate
-        </Link>
-        ,{" "}
-        <Link href="https://vercel.com/templates/ai" target="_blank">
-          Vercel
-        </Link>
-        , and{" "}
-        <Link href="https://upload.io" target="_blank">
-          Upload
-        </Link>
+    
         .
       </div>
     </footer>
