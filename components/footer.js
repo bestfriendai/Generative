9bf75a4c8bbd7ebaca9c45b87b15c9076eb3c8ce
bestfriendai <120160378+bestfriendai@users.noreply.github.com>
2023-03-01 19:20:38 -0500

    Update footer.js

--- components/footer.js
+++ components/footer.js
@@ -3,35 +3,7 @@ import Link from "next/link";
 export default function Footer({ events }) {
   return (
     <footer className="w-full my-8">
-      <div className="text-center lil-text mt-8">
-        <div className="inline-block py-3 px-4 border bg-gray-200 rounded-lg">
-          🍿 Want to build an app like this?{" "}
-          <Link
-            href="https://github.com/replicate/scribble-diffusion"
-            target="_blank"
-          >
-            Fork it on GitHub
-          </Link>{" "}
-          or check out the{" "}
-          <Link href="https://youtu.be/6z07OdbrWOs" target="_blank">
-            video tutorial
-          </Link>
-          .
-        </div>
-      </div>
 
-      <div className="text-center text-sm mt-8">
-        <div className="inline-block py-3 px-4 border border-red-200 bg-red-100 text-orange-900 rounded-lg">
-          🐞 The canvas has some bugs on iOS and Firefox.{" "}
-          <Link
-            href="https://github.com/replicate/scribble-diffusion/issues/31"
-            target="_blank"
-            className="text-orange-900 underline"
-          >
-            Want to help fix it?
-          </Link>
-        </div>
-      </div>
 
       <div className="text-center lil-text mt-8">
         Powered by{" "}
